0c96d1e2cc3e50de991ce5ea3caaeb2b41b208d6
Jumachaje <73429193+Jumanjigobez@users.noreply.github.com>
2024-08-21 19:30:19 +0300

    Updated the user id and reg date format

--- PHP/signup.php
+++ PHP/signup.php
@@ -10,9 +10,9 @@
    
 
     function generateUserID() {
-        $datePart = date("Ymd");
+        // $datePart = date("Ymd");
         $randomPart = generateRandomString(8);
-        return "user_".$randomPart . $datePart;
+        return "user_".$randomPart;
     }
     
     function generateRandomString($length) {
@@ -25,13 +25,13 @@
         return $randomString;
     }
 
-    function getRegDate($userId) {
+    // function getRegDate($userId) {
         
-        $RegDate = substr($userId, -8);
-        if (substr($RegDate, 0, 1) === "2") {
-            return $RegDate; // Outputs: 20240609
-        }
-    }
+    //     $RegDate = substr($userId, -8);
+    //     if (substr($RegDate, 0, 1) === "2") {
+    //         return $RegDate; // Outputs: 20240609
+    //     }
+    // }
 
     if(!empty($username) && !empty($psk) && !empty($email) && $terms_check){
         $query = $conn->prepare("SELECT * FROM `users` WHERE username = (?) OR email = (?)");
@@ -46,7 +46,8 @@
            echo "Account Exists";
         }else{
             $user_id = generateUserID();
-            $reg_date = getRegDate($user_id);
+            date_default_timezone_set('Africa/Nairobi');
+            $reg_date = date('Y-m-d H:i:s');
             $user_type = "normal";
             $status = "offline";
             $terms_check = "Agreed";

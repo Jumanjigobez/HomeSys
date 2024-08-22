84fea3e641bec4b8d6834ae96235344913b80f61
Jumachaje <73429193+Jumanjigobez@users.noreply.github.com>
2024-08-22 18:58:59 +0300

    Update the layering of users and Hashing Psk

--- PHP/admin/editUser.php
+++ PHP/admin/editUser.php
@@ -10,12 +10,13 @@
     $user_type = $_POST['UserType'];
     $terms_agreed = $_POST['TermsAgreed'];
    
+    $psk_hashed = password_hash($psk,PASSWORD_DEFAULT);//Hashed Password
 
     $query = $conn->prepare("UPDATE users SET 
     username = (?), email = (?), psk = (?),
     status = (?), user_type = (?), terms_agreed = (?) WHERE user_id = (?)");
 
-    $query->bind_param("sssssss", $username, $email, $psk, $status, $user_type, $terms_agreed, $user_id);
+    $query->bind_param("sssssss", $username, $email, $psk_hashed, $status, $user_type, $terms_agreed, $user_id);
     $query->execute();
 
     if ($query){
